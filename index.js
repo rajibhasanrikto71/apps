bf2ccbfceaa2848c3f7f7ef92aefb620800066ea
Villain <83833885+rajibhasanrikto71@users.noreply.github.com>
2022-05-21 19:53:33 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+x = document.querySelector("title");
+a = document.querySelector(".but1");
+b = document.querySelector(".but2");
+c = document.querySelector(".but3");
+d = document.querySelector(".but4");
+e = document.querySelector(".but5");
+f = document.querySelector(".but6");
+g = document.querySelector(".but7");
+h = document.querySelector(".but8");
+
+
+x.innerHTML="R R R";
+a.innerHTML="Liker";
+b.innerHTML="Snaptube";
+c.innerHTML="Vidmate";
+
+
